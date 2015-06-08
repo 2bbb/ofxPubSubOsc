a5190b8773f8c54dcc0c362c7362d447775cfffe
ISHII 2bit <ishii@buffer-renaiss.com>
2015-06-08 11:52:29 +0900

    add operator==/!= about ofMatrix3x3, ofMatrix4x4

--- src/details/ofxpubsubosc_type_utils.h
+++ src/details/ofxpubsubosc_type_utils.h
@@ -64,3 +64,24 @@ template <typename T>
 bool operator!=(const ofColor_<T> &x, const ofColor_<T> &y) {
     return x != y;
 }
+
+bool operator==(const ofMatrix3x3 &x, const ofMatrix3x3 &y) {
+    return (x.a == y.a) && (x.b == y.b) && (x.c == y.c)
+        && (x.d == y.d) && (x.e == y.e) && (x.f == y.f)
+        && (x.g == y.g) && (x.h == y.h) && (x.i == y.i);
+}
+
+bool operator!=(const ofMatrix3x3 &x, const ofMatrix3x3 &y) {
+    return !operator==(x, y);
+}
+
+bool operator==(const ofMatrix4x4 &x, const ofMatrix4x4 &y) {
+    return (x._mat[0][0] == y._mat[0][0]) && (x._mat[0][1] == y._mat[0][1]) && (x._mat[0][2] == y._mat[0][2]) && (x._mat[0][3] == y._mat[0][3])
+        && (x._mat[1][0] == y._mat[1][0]) && (x._mat[1][1] == y._mat[1][1]) && (x._mat[1][2] == y._mat[1][2]) && (x._mat[1][3] == y._mat[1][3])
+        && (x._mat[2][0] == y._mat[2][0]) && (x._mat[2][1] == y._mat[2][1]) && (x._mat[2][2] == y._mat[2][2]) && (x._mat[2][3] == y._mat[2][3])
+        && (x._mat[3][0] == y._mat[3][0]) && (x._mat[3][1] == y._mat[3][1]) && (x._mat[3][2] == y._mat[3][2]) && (x._mat[3][3] == y._mat[3][3]);
+}
+
+bool operator!=(const ofMatrix4x4 &x, const ofMatrix4x4 &y) {
+    return !operator==(x, y);
+}
