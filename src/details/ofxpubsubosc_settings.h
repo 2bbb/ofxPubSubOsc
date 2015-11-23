ca834c4d9f338fe900df2b3c1fa0d62d776a432d
ISHII 2bit <ishii@buffer-renaiss.com>
2015-11-23 15:57:07 +0900

    update macro

--- src/details/ofxpubsubosc_settings.h
+++ src/details/ofxpubsubosc_settings.h
@@ -8,7 +8,7 @@
 #ifndef ofxPubSubDevelopProject_ofxpubsubosc_settings_h
 #define ofxPubSubDevelopProject_ofxpubsubosc_settings_h
 
-#if (OF_VERSION_MAJOR == 0) && (OF_VERSION_MINOR < 8) && (OF_VERSION_PATCH < 2)
+#if (OF_VERSION_MAJOR == 0) && ((OF_VERSION_MINOR < 8) || ((OF_VERSION_MINOR == 8) && (OF_VERSION_PATCH < 2)))
 #   define ENABLE_OF_BUFFER 0
 #else
 #   define ENABLE_OF_BUFFER 1
