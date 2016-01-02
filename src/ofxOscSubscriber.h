00b9b1ce2b6b039a29e40a63fcd14527b5de0ba4
ISHII 2bit <ishii@buffer-renaiss.com>
2016-01-02 19:09:52 +0900

    oh yeah!!!! welcome to the future!!

--- src/ofxOscSubscriber.h
+++ src/ofxOscSubscriber.h
@@ -45,6 +45,28 @@ namespace ofx {
         template <typename T>
         struct function_info : public function_info_impl<T, is_callable<T>::value> {};
         
+        template <typename U, typename ret, typename ... arguments>
+        struct function_info<ret(U::*)(arguments ...) const> {
+            static constexpr bool is_function = true;
+            static constexpr std::size_t arity = sizeof...(arguments);
+            using result_type = ret;
+            using arguments_types_tuple = std::tuple<arguments ...>;
+            template <std::size_t index>
+            using argument_type = get_type<std::tuple_element<index, arguments_types_tuple>>;
+            using function_type = std::function<result_type(arguments ...)>;
+        };
+        
+        template <typename U, typename ret, typename ... arguments>
+        struct function_info<ret(U::*)(arguments ...)> {
+            static constexpr bool is_function = true;
+            static constexpr std::size_t arity = sizeof...(arguments);
+            using result_type = ret;
+            using arguments_types_tuple = std::tuple<arguments ...>;
+            template <std::size_t index>
+            using argument_type = get_type<std::tuple_element<index, arguments_types_tuple>>;
+            using function_type = std::function<result_type(arguments ...)>;
+        };
+        
         template <typename ret, typename ... arguments>
         struct function_info<ret(*)(arguments ...)> {
             static constexpr bool is_function = true;
