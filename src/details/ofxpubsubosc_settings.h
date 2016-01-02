4169f142ea657555510599289c20e4a2e6af4501
ISHII 2bit <ishii@buffer-renaiss.com>
2016-01-02 15:46:46 +0900

    add versioning macro

--- src/details/ofxpubsubosc_settings.h
+++ src/details/ofxpubsubosc_settings.h
@@ -8,6 +8,14 @@
 #ifndef ofxPubSubDevelopProject_ofxpubsubosc_settings_h
 #define ofxPubSubDevelopProject_ofxpubsubosc_settings_h
 
+#pragma mark versioning tags
+
+#define OFX_PUBSUBOSC_VERSION_MAJOR 0
+#define OFX_PUBSUBOSC_VERSION_MINOR 2
+#define OFX_PUBSUBOSC_VERSION_PATCH 0
+
+#define OFX_PUBSUBOSC_MULTISUBSCRIBE 1
+
 #if (OF_VERSION_MAJOR == 0) && ((OF_VERSION_MINOR < 8) || ((OF_VERSION_MINOR == 8) && (OF_VERSION_PATCH < 2)))
 #   define ENABLE_OF_BUFFER 0
 #else
